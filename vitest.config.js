e60712645b4e22414c08d0620a508309def3216d
Glory Wong <glorywong1001@gmail.com>
2026-04-29 00:16:23 +0800

    fix: upgrade deps and support node at least 24

--- vitest.config.js
+++ vitest.config.js
@@ -4,7 +4,7 @@ export default defineConfig({
   test: {
     coverage: {
       provider: 'istanbul',
-      include: ['**'],
+      include: ['index.ts'],
       reporter: ['text', 'json-summary', 'json'],
     },
   },
